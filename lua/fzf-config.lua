b8c87f1acaecb9672258c3cb48cc0d51568d2e00
nghiatrandtvt <nghiatrandtvt@gmail.com>
2025-03-04 22:31:39 +0100

    Update fzf-config.lua

--- lua/fzf-config.lua
+++ lua/fzf-config.lua
@@ -1,4 +1,14 @@
-vim.keymap.set("n", "<leader>sb", function()
+
+
+vim.keymap.set("n", "<leader>sB", function()
+  local full_word = vim.fn.expand("<cword>")
+  local word = full_word:match("^[^/]+") or full_word
+  local fzf_options = "--color=hl:yellow,hl+:cyan --query " .. vim.fn.shellescape(word)
+  vim.fn["fzf#vim#lines"](word, {
+    ["sink"] = "e",
+    ["options"] = fzf_options,
+  })
+end, { desc = "FzF-search-under-cursor-all-buf" })vim.keymap.set("n", "<leader>sb", function()
   local full_word = vim.fn.expand("<cword>")
   local word = full_word:match("^[^/]+") or full_word
   local fzf_options = "--color=hl:yellow,hl+:cyan --query " .. vim.fn.shellescape(word)
