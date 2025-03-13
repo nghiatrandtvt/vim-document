5952b0414c605d609651d7afb77c232770c5e26f
nghiatrandtvt <nghiatrandtvt@gmail.com>
2025-03-13 09:46:01 +0100

    Create nvim-tree config for nvim-tree plugin

---
+++ lua/nvim-tree-config.lua
@@ -0,0 +1,14 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
