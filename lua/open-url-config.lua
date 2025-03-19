4a9b47df4ab270786f0ad45f2c416990e74f6e4d
nghiatrandtvt <nghiatrandtvt@gmail.com>
2025-03-19 21:22:46 +0100

    Add file lua to open selected url

---
+++ lua/open-url-config.lua
@@ -0,0 +1,26 @@
+vim.keymap.set("v", "<leader>ju", function()
+  vim.cmd('normal! "zy')
+  local url = vim.fn.getreg('z')
+ 
+  if url == "" then
+    print("No URL selected")
+    return
+  end
+ 
+  url = vim.trim(url)
+ 
+  local open_cmd
+  if vim.fn.has("linux") == 1 then
+    open_cmd = "xdg-open"
+  elseif vim.fn.has("mac") == 1 then
+    open_cmd = "open"
+  elseif vim.fn.has("win32") == 1 then
+    open_cmd = "start"
+  else
+    print("Unsupported OS")
+    return
+  end
+ 
+  print("Opening: " .. url)
+  vim.fn.jobstart({open_cmd, url}, {detach = true})
+end, { desc = "Open-selected-URL-in-browser" })
