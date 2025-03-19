9a068d28a3e2195202d038fd71b18074c58a5991
nghiatrandtvt <nghiatrandtvt@gmail.com>
2025-03-19 12:33:43 +0100

    Add more key mapping to serach file

--- lua/fzf-config.lua
+++ lua/fzf-config.lua
@@ -16,6 +16,21 @@ vim.keymap.set("n", "<leader>sct", function()
   })
 end, { desc = "FzF-search-under-cursor-all-buf" })
 
+vim.keymap.set("n", "<leader>scf", function()
+  local full_path = vim.fn.expand("<cfile>")
+  if full_path == "" then
+    print("No file path under cursor")
+    return
+  end
+  
+  local filename = full_path:match("[^/]+$") or full_path
+  local fzf_options = "--color=hl:yellow,hl+:cyan --query " .. vim.fn.shellescape(filename)
+
+  vim.fn["fzf#vim#files"]("", {
+    options = fzf_options,
+  }, 0)
+end, { desc = "FZF-search-file-under-cursor-current-dir" })
+
 vim.keymap.set("v", "<leader>ss", function()
   vim.cmd('normal! "zy')
   local selected_text = vim.fn.getreg('z')
@@ -33,3 +48,12 @@ vim.keymap.set("v", "<leader>st", function()
     ["options"] = fzf_options,
   })
 end, { desc = "FZF-search-selected-text-all-buf" })
+
+vim.keymap.set("v", "<leader>sf", function()
+  vim.cmd('normal! "zy')
+  local selected_file = vim.fn.getreg('z')
+  local fzf_options = "--color=hl:yellow,hl+:cyan --query " .. vim.fn.shellescape(selected_file)
+  vim.fn["fzf#vim#lines"]("", {
+    ["options"] = fzf_options,
+  }, 0)
+end, { desc = "FZF-search-file-current-dir" })
