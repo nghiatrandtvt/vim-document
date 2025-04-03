f3b9b22834adfbb15f6f458f3fa710119954d635
nghiatrandtvt <nghiatrandtvt@gmail.com>
2025-04-03 12:08:44 +0200

    update nvim treesitter

--- lua/nvim-treesitter-config.lua
+++ lua/nvim-treesitter-config.lua
@@ -1,5 +1,11 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = "all",
-  highlight = { enable = true },
-  indent = { enable = true },
+require("nvim-treesitter.configs").setup {
+  ensure_installed = { "lua", "python", "c", "javascript", "java", "bash", "cpp" },
+  auto_install = true,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+  fold = {
+    enable = true,
+  },
 }
