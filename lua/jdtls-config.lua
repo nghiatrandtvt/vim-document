f0067234be1d94ee22548608db2a6a5626b1734b
nghiatrandtvt <nghiatrandtvt@gmail.com>
2025-04-24 20:22:30 +0200

    Update jdtls-config.lua

--- lua/jdtls-config.lua
+++ lua/jdtls-config.lua
@@ -1,8 +1,11 @@
 local function setup_jdtls()
   local jdtls = require('jdtls')
+  local root_markers = { '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' }
+  local root_dir = require('jdtls.setup').find_root(root_markers)
   local config = {
     cmd = { "jdtls" }, -- can use full path to jdtls. If only jdtls, need to set PATH
-    root_dir = vim.fn.getcwd(),
+    --root_dir = vim.fn.getcwd(),
+	root_dir = root_dir,
     settings = {
       java = {
         home = "<JAVA_HOME>",
