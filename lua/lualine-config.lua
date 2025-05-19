7ae73b89e5f7d2b29576379e746e975a1d738af9
nghiatrandtvt <nghiatrandtvt@gmail.com>
2025-05-19 11:42:14 +0200

    Update lualine-config.lua

--- lua/lualine-config.lua
+++ lua/lualine-config.lua
@@ -2,12 +2,17 @@ require("nvim-web-devicons").setup {
   default = true,
 }
 
+local function get_window_number()
+  return "wnu-" .. vim.api.nvim_win_get_number(0)
+end
+
 require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'powerline_dark',
     section_separators = { left = '', right = '' },
-    component_separators = { left = '', right = '' }
+    component_separators = { left = '', right = '' },
+    globalstatus = true,
   },
   sections = {
     lualine_a = {'mode'},
@@ -15,7 +20,7 @@ require('lualine').setup {
         {'branch', icon = {'',align='left', color={fg='yellow', gui = 'bold'}}}
     },
     lualine_c = {'filename'},
-    lualine_x = {'encoding', 'filetype'},
+    lualine_x = {get_window_number, 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
   }
