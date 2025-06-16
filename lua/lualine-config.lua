23d282755683f3c02bf73927e18bf5e36c1521ff
nghiatrandtvt <nghiatrandtvt@gmail.com>
2025-06-16 17:48:49 +0200

    Update lualine-config.lua

--- lua/lualine-config.lua
+++ lua/lualine-config.lua
@@ -6,13 +6,17 @@ local function get_window_number()
   return "wnu-" .. vim.api.nvim_win_get_number(0)
 end
 
+local custom_powerline_dark = require('lualine.themes.powerline_dark')
+custom_powerline_dark.normal.c.bg = '#05546f'
+custom_powerline_dark.command.c.bg = '#140689'
+
 require('lualine').setup {
   options = {
     icons_enabled = true,
-    theme = 'powerline_dark',
+    theme = custom_powerline_dark,
     section_separators = { left = '', right = '' },
     component_separators = { left = '', right = '' },
-    globalstatus = true,
+	globalstatus = true,
   },
   sections = {
     lualine_a = {'mode'},
