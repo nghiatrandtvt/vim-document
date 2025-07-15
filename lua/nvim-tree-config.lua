1d951769bbd3732eb8c88163d7ad2000eb152bda
nghiatrandtvt <nghiatrandtvt@gmail.com>
2025-07-15 12:13:11 +0200

    Update nvim-tree-config.lua

--- lua/nvim-tree-config.lua
+++ lua/nvim-tree-config.lua
@@ -2,6 +2,11 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 require("nvim-tree").setup({
+  sync_root_with_cwd = true,
+  update_focused_file = {
+    enable = true,
+    update_root = false,
+  },
   sort = {
     sorter = "case_sensitive",
   },
