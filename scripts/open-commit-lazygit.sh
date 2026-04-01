a6e23643d345f2a725aa8d4e1ee1dacf4308dcde
nghiatrandtvt <nghiatrandtvt@gmail.com>
2026-04-01 22:28:49 +0200

    Modify URL construction in open-commit-lazygit.sh
    Update the script to use Change-Id instead of commit hash for URL.

--- scripts/open-commit-lazygit.sh
+++ scripts/open-commit-lazygit.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-BASE_URL='...'
+BASE_URL='...' # Add specific url here
 XDG_COMMAND='xdg-open' #use for linux, window and mac uses difference one
 
 commit_hash=$1
 change_id=$(git show $commit_hash | awk '/Change-Id/ {print $2}')
 
-url="$BASE_URL"/"$commit_hash"
+url="$BASE_URL"/"$change_id"
 echo "Openning $url"
 
 $XDG_COMMAND $url
