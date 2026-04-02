53e45e91852e737592a0a13a6d14872802924ab1
Nghia Tran <nghiatrandtvt@gmail.com>
2026-04-02 15:22:24 +0200

    Add more git commands

--- lua/git_gerrit.lua
+++ lua/git_gerrit.lua
@@ -1,4 +1,45 @@
+-- Copy a commit url to clipboard
+-- Required input: a commit hash must be copied into clipboard somehow in advance
+-- then run <leader>gc to open a commit url from browser
+-- For example:
+--   Do <leader>gB -> yiw (at the short commit hash) -> <leader>gc
+-- This keymap is not for lazygit because it's not inside lazygit
 vim.keymap.set('n', '<leader>gc', function()
 	local hash = vim.fn.getreg('+')
 	vim.fn.system = '<copy-commit-lazygit.sh fullpath> ' .. hash
 end, { desc = 'copy full url to clipboard' })
+
+-- Open a commit url on browser directly
+-- Required input: a commit hash must be copied into clipboard somehow in advance
+-- then run <leader>go to open a commit url from browser
+-- For example:
+--   Do <leader>gB -> yiw (at the short commit hash) -> <leader>go
+-- This keymap is not for lazygit because it's not inside lazygit
+vim.keymap.set('n', '<leader>go', function()
+	local hash = vim.fn.getreg('+')
+	vim.fn.system = '<open-commit-lazygit.sh fullpath> ' .. hash
+end, { desc = 'open a commit on browser' })
+
+-- Open a commit on browser directly
+-- That means you stay on a line on editor and press Ctrl-g to open a commit for that line
+-- This keymap primarily works base on blame feature of gitsigns plugin
+-- It's not for lazygit because it's not inside lazygit
+vim.keymap.set('n', '<c-g>', function()
+  local blame = vim.b.gitsigns_blame_line_dict
+  if blame then
+    local hash = blame.sha
+    vim.fn.system('<open-git-url-script> ' .. hash)
+  end
+end, { desc = 'open commit url from git blame' })
+
+-- Copy a commit url to clipboard
+-- That means you stay on a line on editor and press Alt-g to copy a commit url
+-- This keymap primarily works base on blame feature of gitsigns plugin
+-- It's not for lazygit because it's not inside lazygit
+vim.keymap.set('n', '<a-g>', function()
+  local blame = vim.b.gitsigns_blame_line_dict
+  if blame then
+    local hash = blame.sha
+    vim.fn.system('<copy-git-url-script> ' .. hash)
+  end
+end, { desc = 'copy commit url from git blame' })
--- lua/open-url-config.lua
+++ lua/open-url-config.lua
@@ -43,34 +43,3 @@ vim.keymap.set("n", "<leader>gO", function()
   print("Opening: " .. url)
   vim.fn.jobstart({open_cmd, url}, {detach = true})
 end, { desc = "Open-Gerrit-home-page" })
-
-vim.keymap.set("n", "<leader>go", function()
-  local changeId = vim.fn.getreg('+')
-  print("Value in clipboard: " .. changeId)
-  
-  if changeId:sub(1, 1) ~= "I" then
-    local cmd = string.format("git show %s | grep 'Change-Id' | awk '{print $2}'", changeId)
-	changeId = vim.fn.system(cmd):gsub("\n", "")
-  end
-  
-  if changeId == "" then
-    print("No changeId")
-	return
-  end
-  url = "<url>/#/q/" .. changeId
- 
-  local open_cmd
-  if vim.fn.has("linux") == 1 then
-    open_cmd = "xdg-open"
-  elseif vim.fn.has("mac") == 1 then
-    open_cmd = "open"
-  elseif vim.fn.has("win32") == 1 then
-    open_cmd = "start"
-  else
-    print("Unsupported OS")
-    return
-  end
- 
-  print("Opening: " .. url)
-  vim.fn.jobstart({open_cmd, url}, {detach = true})
-end, { desc = "Open-Gerrit-with-changeId-or-commit-hash" })
